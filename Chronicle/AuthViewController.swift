f2086acd4789b5c4ac0fc6aa61b2bda9d22beb78
Albin Martinsson <albinmartinsson91@gmail.com>
2016-02-07 15:12:51 +0100

    Tog bort onödiga vyer

--- Chronicle/AuthViewController.swift
+++ Chronicle/AuthViewController.swift
@@ -10,99 +10,20 @@ import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
 
-class AuthViewController: UIViewController, UIPageViewControllerDataSource {
+class AuthViewController: UIViewController {
     
     @IBOutlet weak var facebookButton: UIButton!
     var chronicleBlue = UIColor(red:0.25, green:0.52, blue:1.0, alpha:1.0)
     var facebookBlue = UIColor(red:0.23, green:0.35, blue:0.6, alpha:1.0)
     
     
-    
-    // MARK: - Variables
-    private var pageViewController: UIPageViewController?
-    
-    // Initialize it right away here
-    private let contentImages = ["facebook-icon",
-        "facebook-icon",
-        "facebook-icon",
-        "facebook-icon"];
+
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        createPageViewController()
-        setupPageControl()
     }
     
-    private func createPageViewController() {
-        
-        let pageController = self.storyboard!.instantiateViewControllerWithIdentifier("PageController") as! UIPageViewController
-        pageController.dataSource = self
-        
-        if contentImages.count > 0 {
-            let firstController = getItemController(0)!
-            let startingViewControllers: NSArray = [firstController]
-            pageController.setViewControllers(startingViewControllers as! [UIViewController], direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
-        }
-        
-        
-        pageViewController = pageController
-        addChildViewController(pageViewController!)
-        self.view.addSubview(pageViewController!.view)
-        pageViewController!.didMoveToParentViewController(self)
-    }
-    
-    private func setupPageControl() {
-        let appearance = UIPageControl.appearance()
-        appearance.currentPageIndicatorTintColor = UIColor.whiteColor()
-    }
-    
-    // MARK: - UIPageViewControllerDataSource
-    
-    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
-        
-        let itemController = viewController as! PageItemController
-        
-        if itemController.itemIndex > 0 {
-            return getItemController(itemController.itemIndex-1)
-        }
-        
-        return nil
-    }
-    
-    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
-        
-        let itemController = viewController as! PageItemController
-        
-        if itemController.itemIndex+1 < contentImages.count {
-            return getItemController(itemController.itemIndex+1)
-        }
-        
-        return nil
-    }
-    
-    private func getItemController(itemIndex: Int) -> PageItemController? {
-        
-        if itemIndex < contentImages.count {
-            let pageItemController = self.storyboard!.instantiateViewControllerWithIdentifier("ItemController") as! PageItemController
-            pageItemController.itemIndex = itemIndex
-            pageItemController.imageName = contentImages[itemIndex]
-            return pageItemController
-        }
-        
-        return nil
-    }
-    
-    // MARK: - Page Indicator
-    
-    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
-        return contentImages.count
-    }
-    
-    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
-        return 0
-    }
-
     
     
     override func viewWillAppear(animated: Bool) {
