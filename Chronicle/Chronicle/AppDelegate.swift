703162f6e0d01cfa7955ad96329c73f2682daca5
Sebastian Marcusson <sebastian@dobus.se>
2016-03-12 22:44:26 +0100

    Lade till några exempelplatser

--- Chronicle/Chronicle/AppDelegate.swift
+++ Chronicle/Chronicle/AppDelegate.swift
@@ -8,15 +8,12 @@
 
 import Foundation
 import UIKit
-import CoreLocation
 import SWXMLHash
 import RealmSwift
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    var locationManager = CLLocationManager()
-    var regionsToMonitor = [CLCircularRegion]()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         //Ask user for push permission
@@ -28,16 +25,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         } else {
             application.registerForRemoteNotificationTypes([.Badge, .Sound, .Alert])
         }
-        
-        //Ask for permission to read user location
-        locationManager.requestAlwaysAuthorization()
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.delegate = self
-        locationManager.allowsBackgroundLocationUpdates = true
-        //locationManager.startUpdatingLocation()
-        locationManager.startMonitoringSignificantLocationChanges()
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        
         
        let config = Realm.Configuration(
             // Set the new schema version. This must be greater than the previously used
@@ -52,19 +39,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         // Tell Realm to use this new configuration object for the default Realm
         Realm.Configuration.defaultConfiguration = config
         
+        clearRealm()
+        initItems()
         
-        //Clear Realm
-        do {
-            let realm = try Realm()
-            try realm.write() {
-                realm.deleteAll()
-            }
-        } catch {
-            print("Something went wrong with realm!")
-        }
-        
+        return true
+    }
+    
+    func initItems() {
         let xmlPath = NSBundle.mainBundle().pathForResource("data1", ofType: "xml")
-        
         let data = NSData(contentsOfFile: xmlPath!)
         let xml = SWXMLHash.lazy(data!)
         let xmlItems = xml["result"]["records"]["record"]
@@ -78,9 +60,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         var timeLabel: String? = "";
         
         for items in xmlItems {
-            /*for description in items["rdf:RDF"]["rdf:Description"] {
-                print(description["ns5:presentation"]["pres:item"]["pres:id"].element?.text)
-            }*/
             var datan = items["rdf:RDF"]["rdf:Description"][0]
             
             if let dataId = datan["ns5:presentation"]["pres:item"]["pres:id"].element?.text {
@@ -101,30 +80,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
             
             if let datathumbnail = datan["ns5:thumbnail"].element?.text {
                 thumbnail = datathumbnail
-                //print(datathumbnail)
             } else {
                 thumbnail = ""
             }
             if let dataTimeLabel = datan["ns5:presentation"]["pres:item"]["pres:context"]["pres:timeLabel"].element?.text{
                 timeLabel = dataTimeLabel
-                //print(dataTimeLabel)
-                
             }
-            
             
             var coordniates = datan["ns5:presentation"]["pres:item"]["georss:where"]["gml:Point"]["gml:coordinates"].element!.text!
             
-            //var coordinatesArr = split(coordniates) {$0 == ","}
             var splited = coordniates.characters.split{ $0 == "," }.map(String.init)
-            
             long = Double(splited[0])!
-        
-        
             lat = Double(splited[1])!
             
-            
             let item = Item()
-            
             item.itemId = id!
             item.itemLabel = itemlabel!
             item.itemType = itemType!
@@ -143,187 +112,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
             } catch {
                 print("Something went wrong with realm!")
             }
-            
             let realm = try! Realm()
             let items = realm.objects(Item)
-            //print(items)
-            
             
         }
-        
-        // Override point for customization after application launch.
-        /*guard let
-            xmlPath = NSBundle.mainBundle().pathForResource("data1", ofType: "xml"),
-            data = NSData(contentsOfFile: xmlPath)
-            else { return true}
-        
-        do {
-            let xmlDoc = try AEXMLDocument(xmlData: data)
-            var records = [AEXMLElement]()
-            var count: Int = 0
-            //print(xmlDoc.xmlString)
-            print("Some locations")
-            
-            
-            
-            for item in xmlDoc.root["records"]["record"].all! {
-                records.append(item);
-                //print(count)
-                //print(item);
-                count++
-            }
-            
-            for record in records {
-                let item = Item()
-                //print(record.children)
-                for rec in record.children {
-                    //print(rec.stringValue)
-                }
-            }
-            
-            /*for record in list {
-                let item = Item()
-                for rec in record.children {
-                    //print(rec.attributes["name"]);
-                    if let attr = rec.attributes["name"] {
-                        if attr == "itemId" {
-                            item.itemId = rec.stringValue
-                        }
-                        if attr == "itemLabel" {
-                            item.itemLabel = rec.stringValue
-                        }
-                        if attr == "thumbnail" {
-                            item.thumbnail = rec.stringValue
-                        }
-                        if attr == "itemType" {
-                            item.itemType = rec.stringValue
-                        }
-                        if attr == "itemDescription" {
-                            item.itemDescription = rec.stringValue
-                        }
-                        if attr == "lon" {
-                            item.lng = rec.doubleValue
-                        }
-                        if attr == "lat" {
-                            item.lat = rec.doubleValue
-                        }
-                    }
-                }
-                //Let see if item has long/lat
-                if item.lng == 0.0 {
-                    //print("You do not belong in this app")
-                } else {
-                    //Save item to realm if item has long/lat
-                    do {
-                        let realm = try Realm()
-                        try realm.write() {
-                            realm.add(item)
-                        }
-                    } catch {
-                        print("Something went wrong with realm!")
-                    }
-                }
-            }*/
-            
-        }
-        catch {
-            print("\(error)")
-        }*/
-        
-        return true
     }
     
-    func locationManager(manager: CLLocationManager, monitoringDidFailForRegion region: CLRegion?, withError error: NSError) {
-        print(error.localizedDescription)
-    }
-    
-    func locationManager(manager: CLLocationManager, didStartMonitoringForRegion region: CLRegion) {
-        print("Monitor started")
-        print(region)
-        locationManager.requestStateForRegion(region)
-    }
-    
-    func locationManager(manager: CLLocationManager, didDetermineState state: CLRegionState, forRegion region: CLRegion) {
-        if state == CLRegionState.Inside {
-            print("We are at region")
-        } else {
-            print("We are outside of region")
-        }
-        print(state.rawValue)
-    }
-    
-    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
-        print("DID ENTER REGION")
-        print(region.identifier)
-        sendNotification(region)
-    }
-    
-    func sendNotification(region: CLRegion) {
+    func clearRealm() {
+        //Clear Realm
         do {
             let realm = try Realm()
-            let item = realm.objects(Item).filter("itemId == '\(region.identifier)'")[0]
-            var localn = UILocalNotification()
-            localn.alertBody = "Du hittade en skattkista: \(item.itemLabel)"
-            localn.soundName = UILocalNotificationDefaultSoundName;
-            UIApplication.sharedApplication().presentLocalNotificationNow(localn)
+            try realm.write() {
+                realm.deleteAll()
+            }
         } catch {
             print("Something went wrong with realm!")
         }
-    }
-    
-    func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
-        print("DID EXIT REGION")
-    }
-    
-    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
-        print("newLocation")
-        print(newLocation.coordinate.latitude)
-        for region in regionsToMonitor {
-            locationManager.stopMonitoringForRegion(region)
-        }
-        regionsToMonitor.removeAll()
-        
-        let realm = try! Realm()
-        let items = realm.objects(Item)
-        for item in items {
-            let haversineDiff = haversine(newLocation.coordinate.latitude, lon1: newLocation.coordinate.longitude, lat2: item.lat, lon2: item.lng)
-            do {
-                let realm = try Realm()
-                try realm.write() {
-                    item.haversine = haversineDiff
-                    realm.add(item, update: true)
-                }
-            } catch {
-                print("Something went wrong with realm!")
-            }
-        }
-        
-        let itemSortedByHaversine = items.sort({$0.haversine < $1.haversine})
-        for (index, itemSorted) in itemSortedByHaversine.enumerate() {
-            if index < 20 {
-                let tempRegion = CLCircularRegion(center: CLLocationCoordinate2D(latitude: itemSorted.lat, longitude: itemSorted.lng), radius: 50, identifier: itemSorted.itemId)
-                regionsToMonitor.append(tempRegion)
-            }
-        }
-        
-        for region in regionsToMonitor {
-            locationManager.startMonitoringForRegion(region)
-        }
-    }
-    
-    func haversine(lat1:Double, lon1:Double, lat2:Double, lon2:Double) -> Double {
-        let lat1rad = lat1 * M_PI/180
-        let lon1rad = lon1 * M_PI/180
-        let lat2rad = lat2 * M_PI/180
-        let lon2rad = lon2 * M_PI/180
-        
-        let dLat = lat2rad - lat1rad
-        let dLon = lon2rad - lon1rad
-        let a = sin(dLat/2) * sin(dLat/2) + sin(dLon/2) * sin(dLon/2) * cos(lat1rad) * cos(lat2rad)
-        let c = 2 * asin(sqrt(a))
-        let R = 6372.8
-        
-        return R * c
     }
     
     func applicationWillResignActive(application: UIApplication) {
@@ -347,10 +151,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-    
-/*    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
-        
-    }*/
-
 }
 
--- Chronicle/Chronicle/MapViewController.swift
+++ Chronicle/Chronicle/MapViewController.swift
@@ -8,54 +8,45 @@
 
 import UIKit
 import RealmSwift
-//import RealmMapView
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController, MKMapViewDelegate {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     let treasureTransitionmanager = TreasureTransitionManager()
+    var locationManager = CLLocationManager()
+    var regionsToMonitor = [CLCircularRegion]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initLocationManager()
         mapView.delegate = self
         mapView.showsUserLocation = true
-        //Remove mapdata
         if mapView.subviews.count > 0 {
-            print("has subviews")
-            mapView.subviews.first?.subviews.first?.alpha = 0.0
-            //print(MyUIView)
+            //mapView.subviews.first?.subviews.first?.alpha = 0.0
         }
-        //let location = CLLocationCoordinate2D(latitude: 56.6890642598087, longitude: 12.8339380955437)
-        //let region = MKCoordinateRegionMakeWithDistance(location, 5000.0, 7000.0)
         //mapView.setRegion(region, animated: true)
         
         let realm = try! Realm()
         let items = realm.objects(Item)
-        var annotations = [MKPointAnnotation]();
+        var annotations = [MKPointAnnotation]()
         let user = realm.objects(User).first
-        //print(items)
-        
-        /*let annotation = CustomPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2D(latitude: 56.6890642598087, longitude: 12.8339380955437)
-        annotation.title = "heheh"
-        //annotation.imageName = user?.image as! String
-        self.mapView.addAnnotation(annotation)*/
         
         for item in items {
             let annotation = CustomPointAnnotation()
             annotation.coordinate = CLLocationCoordinate2D(latitude: item.lat, longitude: item.lng)
             annotation.title = item.itemLabel
             annotation.imageName = "treasure-pin"
-            self.mapView.addAnnotation(annotation)
             annotations.append(annotation)
             addRadiusCircle(CLLocation(latitude: item.lat, longitude: item.lng))
+            //self.mapView.addAnnotation(annotation)
         }
         
         //let annotations = getMapAnnotations()
         mapView.addAnnotations(annotations)
         
         
-       // let region = CLCircularRegion(center: annotation.coordinate, radius: 1000, identifier: item)
+        //let region = CLCircularRegion(center: annotation.coordinate, radius: 1000, identifier: item)
         //CLCircularRegion(center: <#T##CLLocationCoordinate2D#>, radius: <#T##CLLocationDistance#>, identifier: <#T##String#>)
         
         
@@ -65,16 +56,24 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         UIApplication.sharedApplication().presentLocalNotificationNow(localn)*/
 
     }
+
+    func initLocationManager() {
+        //Ask for permission to read user location
+        locationManager.requestAlwaysAuthorization()
+        //locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.allowsBackgroundLocationUpdates = true
+        //locationManager.startUpdatingLocation()
+        locationManager.startMonitoringSignificantLocationChanges()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
     
     
     @IBAction func openTreasureModal(sender: AnyObject) {
-        
         let vc = self.storyboard?.instantiateViewControllerWithIdentifier("TreasureViewController") as! TreasureViewController
         vc.modalPresentationStyle = UIModalPresentationStyle.Custom
         vc.transitioningDelegate = treasureTransitionmanager
         self.presentViewController(vc, animated: true, completion: nil)
-        
-        
     }
     
     
@@ -98,11 +97,9 @@ class MapViewController: UIViewController, MKMapViewDelegate {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-        print("Should render")
         if !(annotation is CustomPointAnnotation) {
             return nil
         }
@@ -117,15 +114,112 @@ class MapViewController: UIViewController, MKMapViewDelegate {
             anView!.annotation = annotation
         }
         
-        //Set annotation-specific properties **AFTER**
-        //the view is dequeued or created...
-        
         let cpa = annotation as! CustomPointAnnotation
         anView!.image = UIImage(named: cpa.imageName)
         
         return anView
     }
     
+    // MARK: LocationManager methods
+    func locationManager(manager: CLLocationManager, monitoringDidFailForRegion region: CLRegion?, withError error: NSError) {
+        print(error.localizedDescription)
+    }
+    
+    func locationManager(manager: CLLocationManager, didStartMonitoringForRegion region: CLRegion) {
+        print("Monitor started")
+        print(region)
+        locationManager.requestStateForRegion(region)
+    }
+    
+    func locationManager(manager: CLLocationManager, didDetermineState state: CLRegionState, forRegion region: CLRegion) {
+        if state == CLRegionState.Inside {
+            print("We are at region")
+        } else {
+            print("We are outside of region")
+        }
+        print(state.rawValue)
+    }
+    
+    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("DID ENTER REGION")
+        print(region.identifier)
+        sendNotification(region)
+    }
+    
+    func sendNotification(region: CLRegion) {
+        do {
+            let realm = try Realm()
+            let item = realm.objects(Item).filter("itemId == '\(region.identifier)'")[0]
+            var localn = UILocalNotification()
+            localn.alertBody = "Du hittade en skattkista: \(item.itemLabel)"
+            localn.soundName = UILocalNotificationDefaultSoundName;
+            UIApplication.sharedApplication().presentLocalNotificationNow(localn)
+        } catch {
+            print("Something went wrong with realm!")
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("DID EXIT REGION")
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        print("newLocation")
+        print(self.mapView.userLocationVisible)
+        print(newLocation.coordinate.latitude)
+        let center = CLLocationCoordinate2D(latitude: newLocation.coordinate.latitude, longitude: newLocation.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.mapView.setRegion(region, animated: true)
+        
+        
+        for region in regionsToMonitor {
+            locationManager.stopMonitoringForRegion(region)
+        }
+        regionsToMonitor.removeAll()
+        
+        let realm = try! Realm()
+        let items = realm.objects(Item)
+        for item in items {
+            let haversineDiff = haversine(newLocation.coordinate.latitude, lon1: newLocation.coordinate.longitude, lat2: item.lat, lon2: item.lng)
+            do {
+                let realm = try Realm()
+                try realm.write() {
+                    item.haversine = haversineDiff
+                    realm.add(item, update: true)
+                }
+            } catch {
+                print("Something went wrong with realm!")
+            }
+        }
+        
+        let itemSortedByHaversine = items.sort({$0.haversine < $1.haversine})
+        for (index, itemSorted) in itemSortedByHaversine.enumerate() {
+            if index < 20 {
+                let tempRegion = CLCircularRegion(center: CLLocationCoordinate2D(latitude: itemSorted.lat, longitude: itemSorted.lng), radius: 50, identifier: itemSorted.itemId)
+                regionsToMonitor.append(tempRegion)
+            }
+        }
+        
+        for region in regionsToMonitor {
+            locationManager.startMonitoringForRegion(region)
+        }
+    }
+    
+    func haversine(lat1:Double, lon1:Double, lat2:Double, lon2:Double) -> Double {
+        let lat1rad = lat1 * M_PI/180
+        let lon1rad = lon1 * M_PI/180
+        let lat2rad = lat2 * M_PI/180
+        let lon2rad = lon2 * M_PI/180
+        
+        let dLat = lat2rad - lat1rad
+        let dLon = lon2rad - lon1rad
+        let a = sin(dLat/2) * sin(dLat/2) + sin(dLon/2) * sin(dLon/2) * cos(lat1rad) * cos(lat2rad)
+        let c = 2 * asin(sqrt(a))
+        let R = 6372.8
+        
+        return R * c
+    }
 
     /*
     // MARK: - Navigation
