5dc20357b1c89775da8b933407c74425eb7ddecf
Albin Martinsson <albinmartinsson91@gmail.com>
2016-03-12 15:11:42 +0100

    Added treasure open animation

--- Chronicle/Chronicle/TreasureViewController.swift
+++ Chronicle/Chronicle/TreasureViewController.swift
@@ -11,6 +11,41 @@ import UIKit
 class TreasureViewController: UIViewController {
 
     @IBOutlet weak var modalContainer: UIView!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var treasurechest: UIImageView!
+    
+    override func viewWillAppear(animated: Bool) {
+
+        treasurechest.userInteractionEnabled = true
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: Selector("imageTapped:"))
+        treasurechest.addGestureRecognizer(tapRecognizer)
+        treasurechest.image = UIImage(named: "treasure-closed")
+        
+        UIView.animateWithDuration(1.0, delay:0, options: [.Repeat, .Autoreverse], animations: {
+            
+           // self.treasurechest.layer.position.y = botOriginalY + 90
+           // self.treasurechest.layer.position.y = botOriginalY + 38
+            
+            }, completion: nil)
+        
+        
+    }
+    
+    func imageTapped(gestureRecognizer: UITapGestureRecognizer) {
+        //tappedImageView will be the image view that was tapped.
+        //dismiss it, animate it off screen, whatever.
+        print("CLIIIIICKED")
+        let imageName = "spaceship.pdf"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        self.treasurechest.addSubview(imageView)
+        imageView.center = CGPointMake(treasurechest.frame.size.width  / 2,
+            treasurechest.frame.size.height / 2);
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+
+        treasurechest.image = UIImage(named: "treasure-open")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +58,12 @@ class TreasureViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func closeView(sender: AnyObject) {
+        
+       dismissViewControllerAnimated(true) { () -> Void in
+
+        }
+    }
 
     /*
     // MARK: - Navigation
