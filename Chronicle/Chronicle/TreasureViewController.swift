890b3618f8a599524979989cd020aa68d55e05fc
Johan Sveningsson <johan@dobus.se>
2016-03-13 08:29:33 +0100

    Modifierade data och visar rätt bild vid skattfynd

--- Chronicle/Chronicle/TreasureViewController.swift
+++ Chronicle/Chronicle/TreasureViewController.swift
@@ -46,26 +46,16 @@ class TreasureViewController: UIViewController, UIDynamicAnimatorDelegate{
         //tappedImageView will be the image view that was tapped.
         //dismiss it, animate it off screen, whatever.
         print("Opened Chest")
-        let imageName = "spaceship.pdf"
-        var image = UIImage(named: imageName)
+        var image = UIImage()
         let url = NSURL(string: item.thumbnail)
-        var imageDownloader = SDWebImageDownloader()
         if item.thumbnail.isEmpty {
-            image = UIImage(named: "image-placeholder")
+            image = UIImage(named: "image-placeholder")!
         } else {
-            //image = url
-            imageDownloader.downloadImageWithURL(
-                NSURL(string: item.thumbnail),
-                options: SDWebImageDownloaderOptions.UseNSURLCache,
-                progress: nil,
-                completed: { (img, data, error, bool) -> Void in
-                    if img != nil {
-                        image = img
-                    }
-            })
+            let data = NSData(contentsOfURL: url!)
+            image = UIImage(data: data!)!
         }
         
-        let imageView = UIImageView(image: image!)
+        let imageView = UIImageView(image: image)
         self.treasurechest.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 70, height: 70)
         imageView.layer.cornerRadius = 4
