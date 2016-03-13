cf60a441d7bf6b557450dedb82f08db974c5ef32
Albin Martinsson <albinmartinsson91@gmail.com>
2016-03-13 09:23:59 +0100

    Fixade allt

--- Chronicle/Chronicle/DetailViewController.swift
+++ Chronicle/Chronicle/DetailViewController.swift
@@ -17,23 +17,10 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var detailName: UILabel!
     @IBOutlet weak var detailInfoContainer: UIView!
     @IBOutlet weak var detailPhotoBg: UIView!
-    @IBOutlet weak var yearText: UILabel!
     let primaryBlue = UIColor(red:0.22, green:0.52, blue:0.91, alpha:1.0)
     
     @IBOutlet weak var itemDetail: UILabel!
     var itemId: String = ""
-    
-    @IBOutlet weak var childrenSectionLabel: UIView!
-    @IBOutlet weak var childrenSectionText: UILabel!
-    @IBOutlet weak var childrenThen: UILabel!
-    @IBOutlet weak var childrenNow: UILabel!
-   
-    
-    @IBOutlet weak var ageSectionLabel: UIView!
-    @IBOutlet weak var ageSectionText: UILabel!
-    @IBOutlet weak var ageThen: UILabel!
-    @IBOutlet weak var ageNow: UILabel!
-    
     
     
     @IBOutlet weak var detailSmallLabel: UIView!
@@ -43,12 +30,6 @@ class DetailViewController: UIViewController {
         
         detailSmallLabel.backgroundColor = primaryBlue
         detailSmallLabel.layer.cornerRadius = 12
-        
-        childrenSectionLabel.backgroundColor = primaryBlue
-        childrenSectionLabel.layer.cornerRadius = 12
-        
-        ageSectionLabel.backgroundColor = primaryBlue
-        ageSectionLabel.layer.cornerRadius = 12
         
         detailInfoContainer.layer.shadowOffset = CGSizeMake(0, -5)
         detailInfoContainer.layer.shadowRadius = 5
@@ -72,9 +53,6 @@ class DetailViewController: UIViewController {
         let realm = try! Realm()
         let item = realm.objects(Item).filter("itemId = '\(itemId)'")
         let url = NSURL(string: item[0].thumbnail)
-        let childMortality = realm.objects(ChildMortality)
-        //childrenThen.text = childMortality.deathCount
-        print(childMortality)
         
         print(url)
         
@@ -92,7 +70,6 @@ class DetailViewController: UIViewController {
         let itemLabelArr = item.first!.timeLabel.characters.split{$0 == " "}.map(String.init)
         detailYearLabel.text = itemLabelArr[1]
         detailSmallLabelText.text = itemLabelArr[1]
-        yearText.text = itemLabelArr[1]
         
     }
     
