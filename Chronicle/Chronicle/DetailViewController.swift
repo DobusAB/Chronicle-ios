8568fb567a24d6efdc6605d184a52e0677842ffa
Albin Martinsson <albinmartinsson91@gmail.com>
2016-03-13 04:35:30 +0100

    Added view for facts

--- Chronicle/Chronicle/DetailViewController.swift
+++ Chronicle/Chronicle/DetailViewController.swift
@@ -12,16 +12,42 @@ import RealmSwift
 class DetailViewController: UIViewController {
     @IBOutlet weak var detailImageView: UIImageView!
     
+    @IBOutlet weak var detailYearLabel: UILabel!
     @IBOutlet weak var detailName: UILabel!
     @IBOutlet weak var detailInfoContainer: UIView!
     @IBOutlet weak var detailPhotoBg: UIView!
+    @IBOutlet weak var yearText: UILabel!
     let primaryBlue = UIColor(red:0.22, green:0.52, blue:0.91, alpha:1.0)
     
     @IBOutlet weak var itemDetail: UILabel!
     var itemId: String = ""
     
+    @IBOutlet weak var childrenSectionLabel: UIView!
+    @IBOutlet weak var childrenSectionText: UILabel!
+    @IBOutlet weak var childrenThen: UILabel!
+    @IBOutlet weak var childrenNow: UILabel!
+   
+    
+    @IBOutlet weak var ageSectionLabel: UIView!
+    @IBOutlet weak var ageSectionText: UILabel!
+    @IBOutlet weak var ageThen: UILabel!
+    @IBOutlet weak var ageNow: UILabel!
+    
+    
+    
+    @IBOutlet weak var detailSmallLabel: UIView!
+    @IBOutlet weak var detailSmallLabelText: UILabel!
     
     override func viewWillAppear(animated: Bool) {
+        
+        detailSmallLabel.backgroundColor = primaryBlue
+        detailSmallLabel.layer.cornerRadius = 12
+        
+        childrenSectionLabel.backgroundColor = primaryBlue
+        childrenSectionLabel.layer.cornerRadius = 12
+        
+        ageSectionLabel.backgroundColor = primaryBlue
+        ageSectionLabel.layer.cornerRadius = 12
         
         detailInfoContainer.layer.shadowOffset = CGSizeMake(0, -5)
         detailInfoContainer.layer.shadowRadius = 5
@@ -59,7 +85,9 @@ class DetailViewController: UIViewController {
         itemDetail.text = item[0].itemType.capitalizedString
 
         let itemLabelArr = item.first!.timeLabel.characters.split{$0 == " "}.map(String.init)
-        print(itemLabelArr[1])
+        detailYearLabel.text = itemLabelArr[1]
+        detailSmallLabelText.text = itemLabelArr[1]
+        yearText.text = itemLabelArr[1]
     }
     
 
